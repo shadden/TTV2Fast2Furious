6f06a9f3c596b6e652a79625f32ace5f36e6f929
Samuel Hadden <shadden1107@gmail.com>
2019-03-04 14:08:52 -0500

    Code source mved to sub-directory

---
+++ ttv2fast2furious/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Tools for TTV analysis and parameter inference."""
+# Find suffix
+import sysconfig
+suffix = sysconfig.get_config_var('EXT_SUFFIX')
+if suffix is None:
+    suffix = ".so"
+
+# Import shared library
+import os
+import warnings
+pymodulepath = os.path.dirname(__file__)
+
+## Version
+#__version__ = c_char_p.in_dll(clibcelmech, "celmech_version_str").value.decode('ascii')
+#
+## Build
+#__build__ = c_char_p.in_dll(clibcelmech, "celmech_build_str").value.decode('ascii')
+#
+## Githash
+#__githash__ = c_char_p.in_dll(clibcelmech, "celmech_githash_str").value.decode('ascii')
+
+from .ttv2fast2furious import PlanetTransitObservations,TransitTimesLinearModels
+from .ttv2fast2furious import MultiplanetSystemBasisFunctionMatrices
+from .ttv2fast2furious import MultiplanetSystemLinearModelAmplitudes
+
+__all__ = ['PlanetTransitObservations','TransitTimesLinearModels','MultiplanetSystemLinearModelAmplitudes']

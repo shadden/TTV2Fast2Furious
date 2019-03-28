1b6f8bdc903f48a6e416c99644109c5881b6029f
Samuel Hadden <shadden1107@gmail.com>
2019-03-28 13:44:53 -0500

    added 'kepler' module to search the Rowe et. al. (2015) transit time table.

---
+++ ttv2fast2furious/kepler.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+from . import PlanetTransitObservations
+
+_pymodulepath = os.path.dirname(__file__)
+_RoweTableFile = _pymodulepath+"/../data/RoweTableDataframe.pkl"
+
+KOITransitTimeDataframe = pd.read_pickle(_RoweTableFile)
+
+def KOISystemObservations(KOINumber):
+    """
+    Retrieve the transit time observations for a KOI system
+
+    Parameters
+    ----------
+    KOINumber : int or str
+        The KOI number of the system to retrieve
+
+    Returns
+    -------
+    observations : Dictionary of 'PlanetTransitObservations'
+        Transit time observations of the KOIs in the system.
+    """
+    KOI = int(KOINumber)
+    if KOI not in KOITransitTimeDataframe.KOI:
+        print("No entry found for KOI%d"%KOI)
+        return None
+    observations = dict()
+    koi_system_dframe = KOITransitTimeDataframe.query("KOI_System=='%d'"%KOI)
+    for koi,obs in koi_system_dframe.groupby('KOI'):
+        observations.update({
+            koi:PlanetTransitObservations(obs.TransitNumber,obs.TransitTime,obs.eTTV)
+            })
+    return observations

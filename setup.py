788d006184464ce5810fda917ce1bfbb9d1cb33d
Samuel Hadden <shadden1107@gmail.com>
2019-07-31 09:58:53 -0400

    Updating version to 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if suffix is None:
     suffix = ".so"
 
 setup(name='ttv2fast2furious',
-    version='0.1.2',
+    version='1.0.0',
     description='Open source tools for TTV analysis and parameter inference.',
     url='http://github.com/shadden/TTV2Fast2Furious',
     author='Sam Hadden',
